ca24e99bcac0b6cec53ad29b884947a6c1891198
Michael Florya <mih2303@mail.ru>
2024-05-27 19:29:49 +0300

    create model for comments

--- blog/app_blog/models.py
+++ blog/app_blog/models.py
@@ -34,6 +34,8 @@ class Post(models.Model):
     published = PublishedManager()  # конкретно-прикладной менеджер
 
     class Meta:
+        verbose_name = 'пост'
+        verbose_name_plural = 'Посты'
         ordering = ['-publish']
         indexes = [
             models.Index(fields=['-publish']),
@@ -48,3 +50,26 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = 'комментарий'
+        verbose_name_plural = 'Комментарии'
+        ordering = ['created']
+        indexes = [
+            models.Index(fields=['created']),
+        ]
+
+    def __str__(self):
+        return f'Комментарий {self.name} на публикацию {self.post}'
