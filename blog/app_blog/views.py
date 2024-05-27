b1e5a5c8a1649efbef87182d0f591c77fbbaa3e6
Michael Florya <mih2303@mail.ru>
2024-05-28 01:15:51 +0300

    improve the search field

--- blog/app_blog/views.py
+++ blog/app_blog/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from django.views.generic import ListView
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from .forms import CommentForm, SearchForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
@@ -20,9 +20,12 @@ def post_list(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
             results = Post.published.annotate(
-                search=SearchVector('title', 'body'),
-            ).filter(search=query)
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(search=search_query).order_by('-rank')
             post_list = results  # Используем результаты поиска для постраничного вывода
 
     # Постраничная разбивка с 3 постами на страницу
