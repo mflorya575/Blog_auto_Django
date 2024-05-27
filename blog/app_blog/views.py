6f04a76b66dee1029afcc611e285c75cc5e7d1d0
Michael Florya <mih2303@mail.ru>
2024-05-27 17:13:35 +0300

    config templates & static with controllers (list, detail & base)

--- blog/app_blog/views.py
+++ blog/app_blog/views.py
@@ -4,9 +4,9 @@ from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
     posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    return render(request, 'blog/list.html', {'posts': posts})
 
 
 def post_detail(request, id):
     post = get_object_or_404(Post, id=id, status=Post.Status.DRAFT)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    return render(request, 'blog/detail.html', {'post': post})
