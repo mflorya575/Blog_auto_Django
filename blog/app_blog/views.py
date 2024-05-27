8a9c656789a90e58b5e0e3a850f60080d26510de
Michael Florya <mih2303@mail.ru>
2024-05-27 19:09:38 +0300

    create canonical urls

--- blog/app_blog/views.py
+++ blog/app_blog/views.py
@@ -7,6 +7,11 @@ def post_list(request):
     return render(request, 'blog/list.html', {'posts': posts})
 
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.DRAFT)
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
     return render(request, 'blog/detail.html', {'post': post})
