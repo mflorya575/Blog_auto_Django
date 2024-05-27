7a24e960aba9655d4e8e3637ad25e78bb047a4b8
Michael Florya <mih2303@mail.ru>
2024-05-27 20:49:40 +0300

    config DB PostgreSQL instead SQLite

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blogauto',
+        'USER': 'blogauto',
+        'PASSWORD': '54321',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
